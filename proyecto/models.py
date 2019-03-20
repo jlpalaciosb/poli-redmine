cfefdceb7c91498f2d8928508438faeba519853c
Ramon Perdomo <ramper95@gmail.com>
2019-03-20 10:06:46 -0300

    IS2-1 Modelos agregados: Miembro de Proyecto, Fase, Flujo

--- proyecto/models.py
+++ proyecto/models.py
@@ -28,11 +28,88 @@ class Proyecto(models.Model):
     usuario_modificador = models.ForeignKey(User, related_name='usuario_contribuyente_modificador')
 
 
+class Cliente(models.Models):
+    ruc = models.IntegerField(verbose_name='RUC')
+    nombre = models.CharField(verbose_name='Nombre y Apellido', max_length=50)
+    direccion = models.CharField(verbose_name='Direccion', max_length=100)
+    pais = models.CharField(verbose_name='Pais', max_length=20)
+    correo = models.EmailField(verbose_name='Correo')
+    telefono = models.CharField(verbose_name='Telefono', max_length=30)
+
+
 class PermisoProyecto(models.Model):
     descripcion = models.CharField(verbose_name='Descripcion', max_length=100, unique=True)
 
 
 class RolDeProyecto(models.Model):
     nombre = models.CharField(verbose_name='Nombre', max_length=50, unique=True)
-    proyecto = models.ForeignKey(Proyecto)  # on_delete no se especifica?
     permisos = models.ManyToManyField('PermisoProyecto')
+    proyecto = models.ForeignKey(Proyecto)  # on_delete no se especifica?
+
+
+class MiembroProyecto(models.Model):
+    usuario = models.ForeignKey(User)
+    rol = models.ManyToManyField('RolDeProyecto')
+
+
+class Fase(models.Model):
+    nombre = models.CharField(verbose_name='Nombre', max_length=20)
+    orden = models.IntegerField(verbose_name='Orden')
+
+
+class Flujo(models.Model):
+    nombre = models.CharField(verbose_name='Nombre', max_length=20)
+    fase = models.ForeignKey(Fase)  # oneToMany???
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
