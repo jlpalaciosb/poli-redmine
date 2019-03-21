fb5715adb9748d9689356c0377547b082e0ac407
Ramon Perdomo <ramper95@gmail.com>
2019-03-21 11:28:10 -0300

    IS2-1  - Remove field sprint from userstory     - Remove field tipoUs from valorcampopersonalizado     - Add field responsable to actividad     - Add field proyecto to flujo     - Add field cliente to proyecto     - Add field proyecto to sprint     - Add field proyecto to tipous     - Add field campoPersonalizado to valorcampopersonalizado     - Alter field tipoUS on campopersonalizado     - Alter field ruc on cliente     - Add comments to each model

--- proyecto/models.py
+++ proyecto/models.py
@@ -9,6 +9,18 @@ ESTADOS_PROYECTO = (('PENDIENTE', 'Pendiente'),
                     )
 
 
+class Cliente(models.Model):
+    """
+    La clase Cliente representa a un cliente de un proyecto específico
+    """
+    ruc = models.CharField(verbose_name='RUC', max_length=100)
+    nombre = models.CharField(verbose_name='Nombre y Apellido', max_length=50)
+    direccion = models.CharField(verbose_name='Direccion', max_length=100)
+    pais = models.CharField(verbose_name='Pais', max_length=20)
+    correo = models.EmailField(verbose_name='Correo')
+    telefono = models.CharField(verbose_name='Telefono', max_length=30)
+
+
 class Proyecto(models.Model):
     """
     La clase Proyecto representa un proyecto creado por algún
@@ -18,7 +30,7 @@ class Proyecto(models.Model):
                               help_text='El nombre del proyecto (debe ser único en el sistema)',
                               unique=True, default='MyAwesomeProject')
     descripcion = models.TextField(verbose_name='Descripcion del Proyecto', null=True, blank=True)
-    # cliente= models.ForeignKey(Cliente,related_name='proyecto_cliente')
+    cliente = models.ForeignKey(Cliente, related_name='proyecto_cliente', null=True)
     fechaInicioEstimada = models.DateField(verbose_name='Fecha de Inicio Estimada', null=True, blank=True)
     fechaFinEstimada = models.DateField(verbose_name='Fecha de Finalizacion Estimada', null=True, blank=True)
     duracionSprint = models.IntegerField(verbose_name='Duracion del Sprint', default=5)
@@ -26,15 +38,6 @@ class Proyecto(models.Model):
     estado = models.CharField(verbose_name='Estado', choices=ESTADOS_PROYECTO, max_length=30, default='PENDIENTE')
     usuario_creador = models.ForeignKey(User, related_name='usuario_contribuyente_creador')
     usuario_modificador = models.ForeignKey(User, related_name='usuario_contribuyente_modificador')
-
-
-class Cliente(models.Model):
-    ruc = models.IntegerField(verbose_name='RUC')
-    nombre = models.CharField(verbose_name='Nombre y Apellido', max_length=50)
-    direccion = models.CharField(verbose_name='Direccion', max_length=100)
-    pais = models.CharField(verbose_name='Pais', max_length=20)
-    correo = models.EmailField(verbose_name='Correo')
-    telefono = models.CharField(verbose_name='Telefono', max_length=30)
 
 
 """
@@ -55,35 +58,57 @@ class MiembroProyecto(models.Model):
 
 
 class Sprint(models.Model):
+    """
+    La clase Sprint representa a un Sprint de un proyecto específico
+    """
     nombre = models.CharField(verbose_name='Nombre', max_length=20)
     duracion = models.IntegerField(verbose_name='Duracion')
     fechaInicio = models.DateField(verbose_name='Fecha de Inicio')
     estado = models.IntegerField(verbose_name='Estado')
     capacidad = models.IntegerField(verbose_name='Capacidad')
     miembros = models.ManyToManyField(User)
+    proyecto = models.ForeignKey(Proyecto, null=True)
 
 
 class Flujo(models.Model):
+    """
+    La clase Flujo representa a un flujo de algun proyecto especifico
+    """
     nombre = models.CharField(verbose_name='Nombre', max_length=20)
+    proyecto = models.ForeignKey(Proyecto, null=True)
 
 
 class Fase(models.Model):
+    """
+    La clase Fase representa a una fase de algun flujo específico
+    """
     flujo = models.ForeignKey(Flujo)
     nombre = models.CharField(verbose_name='Nombre', max_length=20)
     orden = models.IntegerField(verbose_name='Orden')
 
 
 class TipoUS(models.Model):
+    """
+    La clase TipoUS representa a un Tipo de User Story de un proyecto especifico
+    """
     nombre = models.CharField(verbose_name='Nombre', max_length=20)
+    proyecto = models.ForeignKey(Proyecto, null=True)
 
 
 class CampoPersonalizado(models.Model):
-    tipoUS = models.ForeignKey(TipoUS)
+    """
+    La clase CampoPersonalizado representa a un campo personalizado de un
+    Tipo de User Story especifico
+    """
+    tipoUS = models.ForeignKey(TipoUS, null=True)
     campo = models.CharField(verbose_name='Campo Personalizado', max_length=20)
     tipoDeDato = models.CharField(verbose_name='Tipo de Dato', max_length=7)
 
 
 class UserStory(models.Model):
+    """
+    La clase UserStory representa a un User Story de un proyecto especifico
+    """
     descripcionCorta = models.CharField(verbose_name='Descripcion Corta', max_length=50)
     descripcion = models.CharField(verbose_name='Descripcion', max_length=100)
     criterioAceptacion = models.CharField(verbose_name='Criterio de Aceptacion', max_length=100)
@@ -98,26 +123,39 @@ class UserStory(models.Model):
     faseActual = models.IntegerField(verbose_name='Fase actual en el flujo')
     estadoFaseActual = models.IntegerField(verbose_name='Estado actual en la fase actual del flujo')
     proyecto = models.ForeignKey(Proyecto)
-    sprint = models.ForeignKey(Sprint)
 
 
 class ValorCampoPersonalizado(models.Model):
+    """
+    La clase ValorCampoPersonalizado es la representacion del valor asignado
+    a un campo personalizado de un Tipo de User Story especifico
+    en un User Story especifico
+    """
     us = models.ForeignKey(UserStory)
-    tipoUs = models.ForeignKey(TipoUS)
+    campoPersonalizado = models.ForeignKey(CampoPersonalizado, null=True)
     valor = models.CharField(verbose_name='Valor del Campo Personalizado', max_length=100)
 
 
 class UserStorySprint(models.Model):
+    """
+    La clase UserStorySprint es la representacion de un elemento dentro del
+    Sprint Backlog de un Sprint especifico
+    """
     us = models.ForeignKey(UserStory)
     sprint = models.ForeignKey(Sprint)
     miembro = models.ForeignKey(User)
 
 
 class Actividad(models.Model):
+    """
+    La clase Actividad es la representacion de una actividad de un User Story
+    especifico
+    """
     nombre = models.CharField(verbose_name='Nombre', max_length=20)
     descripcion = models.CharField(verbose_name='Descripcion', max_length=100)
+    responsable = models.ForeignKey(User, null=True)
     # archivos adjuntos. como?
     horasTrabajadas = models.IntegerField(verbose_name='Horas Trabajadas')
-    fase = models.ForeignKey(Fase)  # sin relacion directa con el flujo?
+    fase = models.ForeignKey(Fase)
     estado = models.IntegerField(verbose_name='Estado')
     us_sprint = models.ForeignKey(UserStorySprint)
