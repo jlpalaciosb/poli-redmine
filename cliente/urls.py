3540a9560b1862cfe0204f516c031ce45676335b
José Palacios <joseluis19397@gmail.com>
2019-03-28 18:20:03 -0400

    IS2-2 aplicacion cliente creada

---
+++ cliente/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from cliente.views import \
+    ClienteListView, ClienteCreateView, ClienteUpdateView, \
+    ClientePerfilView, ClienteListJson
+
+urlpatterns = [
+    url(r'^$', ClienteListView.as_view(), name='clientes'),
+    url(r'^crear/$', ClienteCreateView.as_view(), name='crear_cliente'),
+    url(r'^list/$', ClienteListJson.as_view(), name='cliente_list_json'),
+    url(r'^(?P<cliente_id>\d+)/editar/$', ClienteUpdateView.as_view(), name='editar_cliente'),
+    url(r'^(?P<cliente_id>\d+)/perfil/$', ClientePerfilView.as_view(), name='perfil_cliente'),
+]
