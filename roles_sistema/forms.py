ae789338f3d03490c0e604fc9881dc987a911013
Joel Florentin <jflor999@gmail.com>
2019-03-29 05:28:49 -0400

    IS2-3 Solucion al problema del migrate con ContentType
    Ya no se usa de manera explicita el ContentType. Y ya genera el problema

--- roles_sistema/forms.py
+++ roles_sistema/forms.py
@@ -4,9 +4,9 @@ from crispy_forms.layout import Submit, HTML, Layout, Fieldset, Row, Div, Column
 from dal import autocomplete
 from django import forms
 from django.forms import ModelForm, DateInput, Form
-from django.contrib.auth.models import Permission
-from proyecto.models import RolAdministrativo, Proyecto
-from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission, User
+from proyecto.models import RolAdministrativo, Proyecto, Cliente
+
 from django.db.models import Q
 from django.forms import ModelMultipleChoiceField
 
@@ -17,10 +17,10 @@ class PermisosModelMultipleChoiceField(ModelMultipleChoiceField):
 class RolSistemaForm(ModelForm):
     permissions = PermisosModelMultipleChoiceField(
                                                 queryset=Permission.objects.filter(
-                                                    Q(content_type=ContentType.objects.get(model='proyecto')) |
-                                                    Q( content_type=ContentType.objects.get(model='user')) |
-                                                    Q( content_type=ContentType.objects.get(model='roladministrativo')) |
-                                                    Q(content_type=ContentType.objects.get(model='cliente'))
+                                                    Q(content_type__app_label=Proyecto._meta.app_label,content_type__model=Proyecto._meta.model_name) |
+                                                    Q(content_type__app_label=User._meta.app_label,content_type__model=User._meta.model_name) |
+                                                    Q(content_type__app_label=RolAdministrativo._meta.app_label,content_type__model=RolAdministrativo._meta.model_name) |
+                                                    Q(content_type__app_label=Cliente._meta.app_label,content_type__model=Cliente._meta.model_name)
                                                 ).exclude(codename='change_proyecto').exclude(codename='delete_proyecto'),
                                                 widget=forms.CheckboxSelectMultiple,
                                                 required=True,
