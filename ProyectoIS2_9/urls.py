7ed1a49247e0914e2e78a3916bcbb9de1bad6a54
José Palacios <joseluis19397@gmail.com>
2019-04-10 17:45:04 -0400

    restringir acceso a superuser y usuarios staff con nuestro login

--- ProyectoIS2_9/urls.py
+++ ProyectoIS2_9/urls.py
@@ -1,26 +1,13 @@
-"""ProyectoIS2_9 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LogoutView
 
 from ProyectoIS2_9 import settings
 from .views import IndexView
-from django.contrib.auth.views import LoginView, LogoutView
+from ProyectoIS2_9.views import NuestroLoginView
+
 
 urlpatterns = [
 
@@ -34,8 +21,9 @@ urlpatterns = [
 
     url(r'^clientes/', include('cliente.urls')),
 
-    url(r'^login/$', LoginView.as_view(template_name='login/login.html', redirect_authenticated_user=True),
-        name='login'),
+    url(r'^login/$', NuestroLoginView.as_view(template_name='login/login.html',
+            redirect_authenticated_user=True), name='login'),
+
     url(r'^logout/$', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
