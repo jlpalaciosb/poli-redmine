149222beda3445e6ff85f616454b6f832db94cab
José Palacios <joseluis19397@gmail.com>
2019-04-11 09:55:45 -0400

    agregar username a lista de usuarios

--- usuario/views.py
+++ usuario/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseForbidden, JsonResponse, HttpResponse
+from django.http import HttpResponseForbidden
 from django.utils.html import escape
 from django.views.generic import TemplateView, DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -35,25 +35,26 @@ class UsuarioListView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView)
         context['crear_button_text'] = 'Nuevo Usuario'
 
         # datatables
-        context['nombres_columnas'] = ['id', 'Nombre','Email']
+        context['nombres_columnas'] = [
+            'id', 'Username', 'Nombres y Apellidos', 'Correo Electrónico'
+        ]
         context['order'] = [1, "asc"]
         context['datatable_row_link'] = reverse('usuario:ver', args=(1,))  # pasamos inicialmente el id 1
         context['list_json'] = reverse('usuario:lista_json')
 
         #Breadcrumbs
-        context['breadcrumb'] = [{'nombre':'Inicio', 'url':'/'},
-                   {'nombre':'Usuarios', 'url': '#'},
-                   ]
-
-
+        context['breadcrumb'] = [
+            {'nombre':'Inicio', 'url':'/'},
+            {'nombre':'Usuarios', 'url': '#'},
+        ]
 
         return context
 
 
 class UsuarioListJson(LoginRequiredMixin, PermissionRequiredMixin, BaseDatatableView):
     model = User
-    columns = ['id', 'username','email']
-    order_columns = ['id', 'username','email']
+    columns = ['id', 'username', 'nombres y apellidos', 'email']
+    order_columns = ['id', 'username', 'nombres y apellidos', 'email']
     max_display_length = 100
     permission_required = (
         'auth.add_user', 'auth.change_user', 'auth.delete_user')
@@ -67,9 +68,9 @@ class UsuarioListJson(LoginRequiredMixin, PermissionRequiredMixin, BaseDatatable
 
     def render_column(self, row, column):
         # We want to render user as a custom column
-        if column == 'username':
+        if column == 'nombres y apellidos':
             # escape HTML for security reasons
-            return escape('{0} {1}'.format(row.first_name, row.last_name))
+            return escape(row.get_full_name())
         else:
             return super(UsuarioListJson, self).render_column(row, column)
 
@@ -110,10 +111,11 @@ class UsuarioCreateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMess
         context['titulo_form_crear'] = 'Insertar Datos del Nuevo Usuario'
 
         # Breadcrumbs
-        context['breadcrumb'] = [{'nombre': 'Inicio', 'url': '/'},
-                                 {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
-                                 {'nombre': 'Crear', 'url': '#'}
-                                 ]
+        context['breadcrumb'] = [
+            {'nombre': 'Inicio', 'url': '/'},
+            {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
+            {'nombre': 'Crear', 'url': '#'}
+        ]
 
         return context
 
@@ -164,11 +166,12 @@ class UsuarioUpdateView(LoginRequiredMixin, PermissionRequiredMixin, SuccessMess
         context['titulo_form_editar_nombre'] = context[UsuarioUpdateView.context_object_name].username
 
         # Breadcrumbs
-        context['breadcrumb'] = [{'nombre': 'Inicio', 'url': '/'},
-                                 {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
-                                 {'nombre': context['usuario'].get_full_name, 'url': reverse('usuario:ver',kwargs=self.kwargs)},
-                                 {'nombre': 'Editar', 'url': '#'},
-                                 ]
+        context['breadcrumb'] = [
+            {'nombre': 'Inicio', 'url': '/'},
+            {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
+            {'nombre': context['usuario'].get_full_name, 'url': reverse('usuario:ver',kwargs=self.kwargs)},
+            {'nombre': 'Editar', 'url': '#'},
+        ]
 
         return context
 
@@ -203,10 +206,11 @@ class UsuarioPerfilView(LoginRequiredMixin, PermissionRequiredMixin, DetailView)
     def get_context_data(self, **kwargs):
         context = super(UsuarioPerfilView, self).get_context_data(**kwargs)
         context['titulo'] = 'Perfil del Usuario'
-        context['breadcrumb'] = [{'nombre': 'Inicio', 'url': '/'},
-                                 {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
-                                 {'nombre': context['usuario'].get_full_name(), 'url': '#'}
-                                 ]
+        context['breadcrumb'] = [
+            {'nombre': 'Inicio', 'url': '/'},
+            {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
+            {'nombre': context['usuario'].get_full_name(), 'url': '#'}
+        ]
 
         return context
 
@@ -231,11 +235,12 @@ class UsuarioEliminarView(LoginRequiredMixin, PermissionRequiredMixin,SuccessMes
     def get_context_data(self, **kwargs):
         context = super(UsuarioEliminarView, self).get_context_data(**kwargs)
         context['titulo'] = 'Eliminar Rol'
-        context['breadcrumb'] = [{'nombre': 'Inicio', 'url': '/'},
-                                 {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
-                                 {'nombre': context['usuario'].username,
-                                  'url': reverse('usuario:ver', kwargs=self.kwargs)},
-                                 {'nombre': 'Eliminar', 'url': '#'}, ]
+        context['breadcrumb'] = [
+            {'nombre': 'Inicio', 'url': '/'},
+            {'nombre': 'Usuarios', 'url': reverse('usuario:lista')},
+            {'nombre': context['usuario'].username, 'url': reverse('usuario:ver', kwargs=self.kwargs)},
+            {'nombre': 'Eliminar', 'url': '#'}
+        ]
         context['eliminable'] = self.eliminable()
         return context
 
