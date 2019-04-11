45f53e871cdb795f6cba3efafb561673efe3879e
José Palacios <joseluis19397@gmail.com>
2019-04-11 12:07:36 -0400

    excluir permiso 'change_proyecto' en RolSistemaForm

--- roles_sistema/forms.py
+++ roles_sistema/forms.py
@@ -16,15 +16,15 @@ class PermisosModelMultipleChoiceField(ModelMultipleChoiceField):
 
 class RolSistemaForm(ModelForm):
     permissions = PermisosModelMultipleChoiceField(
-                                                queryset=Permission.objects.filter(
-                                                    Q(content_type__app_label=Proyecto._meta.app_label,content_type__model=Proyecto._meta.model_name) |
-                                                    Q(content_type__app_label=User._meta.app_label,content_type__model=User._meta.model_name) |
-                                                    Q(content_type__app_label=RolAdministrativo._meta.app_label,content_type__model=RolAdministrativo._meta.model_name) |
-                                                    Q(content_type__app_label=Cliente._meta.app_label,content_type__model=Cliente._meta.model_name)
-                                                ).exclude(codename='delete_proyecto'),
-                                                widget=forms.CheckboxSelectMultiple,
-                                                required=True,
-                                                label="Permisos"
+        queryset=Permission.objects.filter(
+            Q(content_type__app_label=Proyecto._meta.app_label,content_type__model=Proyecto._meta.model_name) |
+            Q(content_type__app_label=User._meta.app_label,content_type__model=User._meta.model_name) |
+            Q(content_type__app_label=RolAdministrativo._meta.app_label,content_type__model=RolAdministrativo._meta.model_name) |
+            Q(content_type__app_label=Cliente._meta.app_label,content_type__model=Cliente._meta.model_name)
+        ).exclude(codename='change_proyecto').exclude(codename='delete_proyecto'),
+        widget=forms.CheckboxSelectMultiple,
+        required=True,
+        label="Permisos"
     )
     class Meta:
         model = RolAdministrativo
