44103cba6ee567f28147694c0c4677fb1f751327
Joel Florentin <jflor999@gmail.com>
2019-04-13 08:57:40 -0400

    IS2-20 Se realizo los cambios de los permisos en los modelos de acuerdo al issue #20

--- roles_sistema/forms.py
+++ roles_sistema/forms.py
@@ -5,7 +5,7 @@ from dal import autocomplete
 from django import forms
 from django.forms import ModelForm, DateInput, Form
 from django.contrib.auth.models import Permission, User
-from proyecto.models import RolAdministrativo, Proyecto, Cliente
+from proyecto.models import RolAdministrativo, Proyecto, Cliente, Usuario
 
 from django.db.models import Q
 from django.forms import ModelMultipleChoiceField
@@ -17,11 +17,11 @@ class PermisosModelMultipleChoiceField(ModelMultipleChoiceField):
 class RolSistemaForm(ModelForm):
     permissions = PermisosModelMultipleChoiceField(
         queryset=Permission.objects.filter(
-            Q(content_type__app_label=Proyecto._meta.app_label,content_type__model=Proyecto._meta.model_name) |
-            Q(content_type__app_label=User._meta.app_label,content_type__model=User._meta.model_name) |
+            Q(content_type__app_label=Proyecto._meta.app_label,content_type__model=Proyecto._meta.model_name, codename='add_proyecto') |
+            Q(content_type__app_label=Usuario._meta.app_label,content_type__model=Usuario._meta.model_name) |
             Q(content_type__app_label=RolAdministrativo._meta.app_label,content_type__model=RolAdministrativo._meta.model_name) |
             Q(content_type__app_label=Cliente._meta.app_label,content_type__model=Cliente._meta.model_name)
-        ).exclude(codename='change_proyecto').exclude(codename='delete_proyecto'),
+        ),
         widget=forms.CheckboxSelectMultiple,
         required=True,
         label="Permisos"
