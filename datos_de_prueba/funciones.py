b886b1523f40d6bfc258d9870d1d74238db4b935
José Palacios <joseluis19397@gmail.com>
2019-04-13 15:43:20 -0400

    (#6) agregar rol administrativo 'Administrador'

--- datos_de_prueba/funciones.py
+++ datos_de_prueba/funciones.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from proyecto.models import Cliente
+from proyecto.models import RolAdministrativo
 
 PASSWORD = '12345' # contraseña para todos los usuarios
 
@@ -19,7 +20,7 @@ def cargar_user(username, first_name, last_name, email):
 def cargar_users():
     cargar_admin()
     cargar_user('juanpe', 'Juan', 'Pérez', 'juanpe@gmail.com')
-    cargar_user('gladys', 'Gladys', 'López', 'gladys@gmail.com')
+    cargar_user('camila', 'Camila', 'Alderete', 'camila@gmail.com')
     cargar_user('moises', 'Moisés', 'Cabrera', 'moises@gmail.com')
 
 
@@ -35,3 +36,26 @@ def cargar_clientes():
     cargar_cliente('1846374-1', 'Clotilde Méndez', 'clotilde@gmail.com', '021 345 987', 'Paraguay', 'dx')
     cargar_cliente('2483729-8', 'Pablo Nacimiento', 'poblonaci@gmail.com', '0961 309 783', 'Paraguay', 'dx')
     cargar_cliente('1453739-0', 'Vent SA', 'contacto@vent.com', '021 339 483', 'Paraguay', 'dx')
+
+
+def permiso(codename):
+    """
+    Retorna objeto permiso con el codename recibido, OJO que no funciona si hay
+    dos permisos con el mismo codename o si no hay permiso con el codename recibido
+    """
+    return Permission.objects.get(codename=codename)
+
+def cargar_rol_administrador():
+    ra = RolAdministrativo(name='Administrador')
+    ra.save()
+    ra = RolAdministrativo.objects.get(name='Administrador')
+    ra.permissions.set([
+        permiso('add_usuario'), permiso('change_usuario'), permiso('delete_usuario'),
+        permiso('add_cliente'), permiso('change_cliente'), permiso('delete_cliente'),
+        permiso('add_roladministrativo'), permiso('change_roladministrativo'), permiso('delete_roladministrativo'),
+        permiso('add_proyecto')
+    ])
+    print(" + Rol 'Administrador' agregado")
+
+def asignar_roles_administrativos():
+    ra = 1
--- datos_de_prueba/poblar.py
+++ datos_de_prueba/poblar.py
@@ -1,4 +1,5 @@
-from datos_de_prueba.funciones import cargar_users, cargar_clientes
+from datos_de_prueba.funciones import cargar_users, cargar_clientes, cargar_rol_administrador
 
 cargar_users()
 cargar_clientes()
+cargar_rol_administrador()
