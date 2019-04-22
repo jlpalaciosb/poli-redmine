71eee833410bd0e0d968f86bd341a03fb1e83cea
José Palacios <joseluis19397@gmail.com>
2019-04-22 09:32:29 -0400

    #38 crear MiembrosBaseTest para que los otros tests extiendan este

--- proyecto/tests/tests_miembroproyecto.py
+++ proyecto/tests/tests_miembroproyecto.py
@@ -9,19 +9,26 @@ PWD = '12345'
 TELEF = '0986738162'
 
 
-class ListaDeMiembrosTest(TestCase):
-    url_name = 'proyecto_miembro_list'
+class MiembrosBaseTest(TestCase):
+    """
+    Test base para las vistas de miembros de proyecto
+    """
+    url = ''
 
     def setUp(self):
         ua = User.objects.create_user('user_a', EMAIL, PWD)
         ub = User.objects.create_user('user_b', EMAIL, PWD)
         uc = User.objects.create_user('user_c', EMAIL, PWD)
+
         ca = Cliente.objects.create(ruc='1234567-1', nombre='cliente_a', correo=EMAIL, telefono=TELEF, pais='px', direccion='dx')
+
         p1 = Proyecto.objects.create(nombre='proyecto_1', cliente=ca, scrum_master=ua)
+
         mb = MiembroProyecto.objects.create(user=ub, proyecto=p1)
-        self.url = reverse(self.url_name, args=(p1.id,))
 
     def test_miembros_pueden_ver(self):
+        if self.url == '': return
+
         self.client.login(username='user_a', password=PWD)
         response = self.client.get(self.url)
         self.assertEqual(200, response.status_code)
@@ -31,12 +38,31 @@ class ListaDeMiembrosTest(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_no_miembros_no_pueden_ver(self):
+        if self.url == '': return
+
         self.client.login(username='user_c', password=PWD)
         response = self.client.get(self.url)
         self.assertEqual(403, response.status_code)
 
-class ListaDeMiembrosJSONTest(ListaDeMiembrosTest):
-    url_name = 'proyecto_miembro_list_json'
+
+class ListaDeMiembrosTest(MiembrosBaseTest):
+    """
+    Test para la vista MiembroProyectoListView
+    """
+    def setUp(self):
+        super().setUp()
+        pid = Proyecto.objects.get(nombre='proyecto_1').id
+        self.url = reverse('proyecto_miembro_list', args=(pid,))
+
+
+class ListaDeMiembrosJSONTest(MiembrosBaseTest):
+    """
+    Test para la vista MiembroProyectoListJson
+    """
+    def setUp(self):
+        super().setUp()
+        pid = Proyecto.objects.get(nombre='proyecto_1').id
+        self.url = reverse('proyecto_miembro_list_json', args=(pid,))
 
     def test_retorna_todos_los_miembros(self):
         self.client.login(username='user_a', password=PWD)
